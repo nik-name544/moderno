82b909918cd586f97c93ee1196412bd5eb09330c
nik-name544 <revenkonik30@gmail.com>
2020-05-25 23:30:10 +0300

    newItems-upd_v0.7

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('css', function (){
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
     ])
         .pipe(concat('_libs.scss'))
         .pipe(cssmin())
@@ -53,6 +54,7 @@ gulp.task('js', function (){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
