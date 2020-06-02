eb5c97fc5ce68d2d396ff8bfd52f6317ae61584c
nik-name544 <revenkonik30@gmail.com>
2020-06-03 00:31:08 +0300

    upd-v1

--- app/js/main.js
+++ app/js/main.js
@@ -35,15 +35,24 @@ $(function () {
         $('.icon-th-list').removeClass('active');
     });
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
     });
 
-    $('.header__btn-menu').on('click', function(){
+    $('.header__btn-menu').on('click', function () {
         $('.header__box').toggleClass('active');
     });
 
-    
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
 
     var mixer = mixitup('.products__inner-box');
 
